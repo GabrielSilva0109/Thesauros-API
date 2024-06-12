40457eb2834c4a69bcc6c80caf87670f5fa295a3
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-12 10:01:26 -0300

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Bem-vindo ao Thesauros!')
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+})
