7f0cc5cf1c015cbfdccf9fe959aa514bd3d65469
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-12 11:08:30 -0300

    1

---
+++ contract.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether, "Entrada requer uma quantia mínima de 0.01 BNB");
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address ; // Limpa o array de jogadores
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Apenas o gerente pode chamar esta função");
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const Web3 = require('web3')
 const app = express()
 const port = process.env.PORT || 3000
 
+const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'))
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Thesauros!')
-})
+    res.send('Welcome to Thesauros BSC!')
+});
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
-})
+});
