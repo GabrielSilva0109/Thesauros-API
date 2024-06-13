f5d22b245f4453c46081ab9fef4584cd12e6c09c
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-13 11:11:20 -0300

    1

---
+++ src/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
+    connectTimeout: 10000
+})
+
+module.exports = connection
