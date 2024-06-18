d1c777a9ab1a5d233f243a8ee6acd0a8ef5d34b8
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-18 09:14:08 -0300

    1

--- index.js
+++ index.js
@@ -6,16 +6,12 @@ const Web3 = require('web3')
 const app = express()
 const port = process.env.PORT || 3000
 
-const userRoutes = require('./routes/UserRoutes')
-
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
      res.send('Welcome to Thesauros!')
 })
-
-app.use('/api', userRoutes)
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
