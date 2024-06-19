e7afc4b47bef9cd9be77d5a8bd59230efd6ae8ce
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-19 09:41:24 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
-require('dotenv').config()
-const mysql = require('mysql2')
+require('dotenv').config();
+const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT || 3306,
-    connectTimeout: 10000
+    connectTimeout: 10000,
+    connectionLimit: 10, 
+    queueLimit: 0 
 })
 
-module.exports = connection
+module.exports = pool;
